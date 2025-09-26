a250699553a7f6b7e1a60a88918aeab2e45d1d3f
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-09-26 01:37:52 -0400

    [.go] 09-26-2025 , 24 ms (50.7%) , 10.2 MB (21.17%)

--- LeetCode/Medium/0015-3sum/0015-3sum.go
+++ LeetCode/Medium/0015-3sum/0015-3sum.go
@@ -1,38 +1,29 @@
-import "sort"
-
 func threeSum(nums []int) [][]int {
-    var results [][]int
-    sort.Ints(nums)
-    for i := 0; i < len(nums); i++ {
-        if i > 0 && nums[i] == nums[i-1] {
-            continue
-        }
-        
-        left := i + 1
-        right := len(nums) - 1
-        
-        for left < right {
-            sum := nums[i] + nums[left] + nums[right]
-            
-            if sum < 0 {
-                left++
-            } else if sum > 0 {
-                right--
-            } else {
-                resSlice := []int{nums[i], nums[left], nums[right]}
-                results = append(results, resSlice)
-                left++
-                right--
-                
-                for left < right && nums[left] == nums[left-1] {
-                    left++
-                }
-                for left < right && nums[right] == nums[right+1] {
-                    right--
-                }
-            }
-        }
-    }
-    
-    return results
+	sort.Ints(nums)
+	finalList := [][]int{}
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue // skip duplicate fixed values
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			if nums[i]+nums[left]+nums[right] == 0 {
+				finalList = append(finalList, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+
+			} else if nums[i]+nums[left]+nums[right] < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return finalList
 }
