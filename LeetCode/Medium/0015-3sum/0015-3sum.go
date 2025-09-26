cb776b26bb1bc447320b81d50e4a83f2ee548919
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-09-26 01:30:03 -0400

    [.go] 09-26-2025 , 25 ms (44.84%) , 9.6 MB (69.45%)

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.go
@@ -0,0 +1,38 @@
+import "sort"
+
+func threeSum(nums []int) [][]int {
+    var results [][]int
+    sort.Ints(nums)
+    for i := 0; i < len(nums); i++ {
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+        
+        left := i + 1
+        right := len(nums) - 1
+        
+        for left < right {
+            sum := nums[i] + nums[left] + nums[right]
+            
+            if sum < 0 {
+                left++
+            } else if sum > 0 {
+                right--
+            } else {
+                resSlice := []int{nums[i], nums[left], nums[right]}
+                results = append(results, resSlice)
+                left++
+                right--
+                
+                for left < right && nums[left] == nums[left-1] {
+                    left++
+                }
+                for left < right && nums[right] == nums[right+1] {
+                    right--
+                }
+            }
+        }
+    }
+    
+    return results
+}
