ce5a57beef42dd29c14b7b1d19be6beda39ec89b
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-10-13 20:19:30 -0400

    [.go] 10-13-2025 , 55 ms (42.7%) , 11.6 MB (63.79%)

---
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
@@ -0,0 +1,35 @@
+func longestConsecutive(nums []int) int {
+	//store all elements to hashset - hashset fast lookup
+	// loop through all nums in nums
+	//check if start of seq i.e. if val-1 exists in nums
+	numsSet := make(map[int]struct{})
+	maxLength := 0
+	for _, val := range nums {
+		numsSet[val] = struct{}{}
+	}
+	for num := range numsSet {
+		if _, exists := numsSet[num-1]; exists {
+			continue
+		} else {
+			currLength := 1
+			currNum := num
+			nextNum := num + 1
+
+			for {
+				if _, exists := numsSet[nextNum]; exists {
+					currNum = nextNum
+					nextNum = currNum + 1
+					currLength++
+				} else {
+					break
+				}
+			}
+				if currLength > maxLength {
+				maxLength = currLength
+			}
+
+		}
+		
+	}
+    return maxLength
+}
