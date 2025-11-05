f56b9ab4231a1901ed7d24201a8aa94c4f96365a
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-11-04 23:13:06 -0500

    [.go] 11-04-2025 , 0 ms (100%) , 5.7 MB (85.1%)

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.go
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.go
@@ -1,20 +1,35 @@
 func canConstruct(ransomNote string, magazine string) bool {
-	freqMap := make(map[rune]int)
-	for _, char := range magazine {
-		if _, ok := freqMap[char]; ok {
-			freqMap[char] += 1
-		} else {
-			freqMap[char] = 1
-		}
-	}
+	// freqMap := make(map[rune]int)
+	// for _, char := range magazine {
+	// 	if _, ok := freqMap[char]; ok {
+	// 		freqMap[char] += 1
+	// 	} else {
+	// 		freqMap[char] = 1
+	// 	}
+	// }
 
-	for _, char := range ransomNote {
-		if count, ok := freqMap[char]; ok && count > 0 {
-			freqMap[char] -= 1
-		} else {
-			return false
-		}
-	}
+	// for _, char := range ransomNote {
+	// 	if count, ok := freqMap[char]; ok && count > 0 {
+	// 		freqMap[char] -= 1
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
 
-	return true
+	// return true
+
+
+    freqSlice := make([]int, 26)
+    for _, char := range magazine{
+        freqSlice[char - 'a'] += 1
+    }
+
+    for _, char := range ransomNote{
+        if freqSlice[char-'a'] > 0{
+            freqSlice[char - 'a'] -= 1
+        }else{
+            return false
+        }
+    }
+    return true
 }
