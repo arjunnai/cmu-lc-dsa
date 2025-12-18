7b0facd36f3982ca715de5cc5bcf154524aa3fe4
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-12-18 16:17:58 -0500

    [.cpp] 12-18-2025 , 79 ms (75.62%) , 194.8 MB (61.13%)

--- LeetCode/Medium/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ LeetCode/Medium/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
         int n = x.size() - 1;
-        vector<int> scores;
+        // vector<int> scores;
         unordered_map<int, int> ump;
         for (int i = 0; i <= n; i++) {
             ump[x[i]] = max(y[i], ump[x[i]]);
         }
+        // for (auto item : ump) {
+        //     scores.push_back(item.second);
+        // }
+        // if (scores.size() < 3) {
+        //     return -1;
+        // }
+        // sort(scores.begin(), scores.end(), greater<int>());
+
+        // return scores[0] + scores[1] + scores[2];
+        priority_queue<int, vector<int>, greater<int>> scores;
         for (auto item : ump) {
-            scores.push_back(item.second);
+            scores.push(item.second);
+            if (scores.size() > 3) {
+                scores.pop();
+            }
         }
         if (scores.size() < 3) {
             return -1;
-        }
-        sort(scores.begin(), scores.end(), greater<int>());
+        } else {
+            int sum = 0;
+            while (!scores.empty()) {
+                cout << scores.top();
+                sum += scores.top();
+                scores.pop();
+            }
 
-        return scores[0] + scores[1] + scores[2];
+            return sum;
+        }
     }
 };
