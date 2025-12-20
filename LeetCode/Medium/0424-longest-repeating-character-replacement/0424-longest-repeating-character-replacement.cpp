74644166c4c8f85880ed1a31c5569b781ac313e9
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-12-19 19:53:47 -0500

    [.cpp] 12-19-2025 , 0 ms (100%) , 10.8 MB (69.17%)

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -9,8 +9,10 @@ public:
         for (int r = 0; r < s.size(); r++) {
             // ump[s[r]]++;
             count[s[r] - 'A']++;
+            // max_freq = max(max_freq, ump[s[r]]);
             max_freq = max(max_freq, count[s[r]-'A']);
             if ((r - l + 1) - max_freq > k) {
+                // ump[s[l]]--;
                 count[s[l]-'A']--;
                 l++;
             }
