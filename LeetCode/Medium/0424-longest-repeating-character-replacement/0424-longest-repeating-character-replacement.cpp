e11eebf821198ede95b8f5e8007d19bee293ee35
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-12-19 19:51:05 -0500

    [.cpp] 12-19-2025 , 0 ms (100%) , 10.7 MB (91.92%)

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> ump;
+        // unordered_map<char, int> ump;
+        vector<int> count(26, 0);
         int max_freq = 0;
         int ans = 0;
         int l = 0;
         for (int r = 0; r < s.size(); r++) {
-            ump[s[r]]++;
-            max_freq = max(max_freq, ump[s[r]]);
+            // ump[s[r]]++;
+            count[s[r] - 'A']++;
+            max_freq = max(max_freq, count[s[r]-'A']);
             if ((r - l + 1) - max_freq > k) {
-                ump[s[l]]--;
+                count[s[l]-'A']--;
                 l++;
             }
             ans = max(ans, (r - l + 1));
