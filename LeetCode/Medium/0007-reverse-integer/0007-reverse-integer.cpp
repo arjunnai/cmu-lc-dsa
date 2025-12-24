b61aedde7f6e154d4b2dbe585af88110b153ccd4
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-12-24 14:43:21 -0500

    [.cpp] 12-24-2025 , 0 ms (100%) , 8.7 MB (20.12%)

---
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0, digit = 0;
+        while (x != 0) {
+            digit = x % 10;
+            x = x / 10;
+            if (ans > INT_MAX / 10 || ans < INT_MIN / 10) {
+            // if (ans * 10 > INT_MAX || ans * 10 < INT_MIN) {
+                return 0;
+            }
+            ans = ans * 10 + digit;
+        }
+        return ans;
+    }
+};
