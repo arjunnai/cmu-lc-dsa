4dcd29c4aa20f72a001e42b2845384f44ea5966c
Arjun <50323916+arjunnai@users.noreply.github.com>
2025-12-30 18:11:57 -0500

    [.cpp] 12-30-2025 , 0 ms (100%) , 22.8 MB (18.1%)

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,15 @@
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int idx = 1;
-        for (int i = 1 ; i < nums.size(); i++){
-            if(nums[i] != nums[i-1]){
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[idx] = nums[i];
                 idx++;
             }
