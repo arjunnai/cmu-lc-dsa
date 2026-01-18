f0c2a14897d46ff28279ac9f0ceb4ec6b289b217
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-01-18 18:44:38 -0500

    [.cpp] 01-18-2026 , 0 ms (100%) , 18.4 MB (95.23%)

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool rowZ = false;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    if (i > 0) {
+                        matrix[i][0] = 0;
+                    } else {
+                        rowZ = true;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 1; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (rowZ) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
