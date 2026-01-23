9aaa46939ec3bf9db528d687aacec653821adc5e
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-01-23 00:51:49 -0500

    [.cpp] 01-23-2026 , 0 ms (100%) , 48.4 MB (80.99%)

--- LeetCode/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ LeetCode/Medium/0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
+        // flip already seen number index to negative, since its from 1 -> n, if
+        // number is 2, idx will be 1
         int idx = 0;
-        for(int i = 0 ; i < nums.size(); i++){
-            idx = abs(nums[i])-1;
-            if(nums[idx] < 0){
+        for (int i = 0; i < nums.size(); i++) {
+            idx = abs(nums[i]) - 1;
+            if (nums[idx] < 0) {
                 res.push_back(abs(nums[i]));
-            }else{
+            } else {
                 nums[idx] = -nums[idx];
             }
         }
