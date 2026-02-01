c9133ccff7e56d446c6810fbb98aaf5d821f45ba
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-01-31 23:44:16 -0500

    [.cpp] 01-31-2026 , 0 ms (100%) , 16.5 MB (30.42%)

--- LeetCode/Medium/0061-rotate-list/0061-rotate-list.cpp
+++ LeetCode/Medium/0061-rotate-list/0061-rotate-list.cpp
@@ -11,6 +11,13 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        // first k remainder size of list gives actual times we need to really
+        // torate the list start off by finding size of list. in the same loop
+        // set the last element to point to head to make it a circular loop
+        // once thats done calculate real k
+        // iterate till count - k since thats where the new tail will be and
+        // then cut it off so that returned pointer points to the new head of the list
+
         int count = 1;
         if (!head) {
             return nullptr;
@@ -20,12 +27,16 @@ public:
             count++;
             current = current->next;
         }
-        current->next = head;
         k = k % count;
+        if(k == 0){
+            return head;
+        }
+        current->next = head;
+
         for (int i = 0; i < count - k; i++) {
             current = current->next;
         }
-        head = current -> next;
+        head = current->next;
         current->next = nullptr;
         return head;
     }
