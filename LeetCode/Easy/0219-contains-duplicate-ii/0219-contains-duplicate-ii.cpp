6e89285ef79c8af7d80ed2cbc8da63ca38812350
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-04 23:55:12 -0500

    [.cpp] 02-04-2026 , 70 ms (77.53%) , 98.6 MB (52.54%)

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> ust;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > k) {
-                ust.erase(nums[i - k - 1]);
+        unordered_map<int, int> ump;
+        for (int r = 0; r < nums.size(); r++) {
+            if (ump.contains(nums[r])) {
+                if (abs(ump[nums[r]] - r) <= k) {
+                    return true;
+                }
             }
-            if (ust.contains(nums[i])) {
-                return true;
-            }
-            ust.insert(nums[i]);
+            ump[nums[r]] = r;
         }
         return false;
     }
 };
+
+// 0 1 2 3 4 5 6
+// 1 0 1 1 k = 1
+// l   r
