0caca4616bcdab7699531dbf5c13843ef3b1fd23
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-04 20:01:21 -0500

    [.cpp] 02-04-2026 , 12 ms (13.37%) , 25.6 MB (91.36%)

--- LeetCode/Easy/3379-transformed-array/3379-transformed-array.cpp
+++ LeetCode/Easy/3379-transformed-array/3379-transformed-array.cpp
@@ -3,9 +3,21 @@ public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         vector<int> res(nums.size());
         int n = nums.size();
-        for(int i = 0 ; i < nums.size(); i++){
-            int idx = (((i+nums[i])%n) + n)%n;
-            res[i] = nums[idx];
+        for (int i = 0; i < nums.size(); i++) {
+            // int idx = (((i+nums[i])%n) + n)%n;
+            // res[i] = nums[idx];
+            int idx = 0;
+            if (nums[i] > 0) {
+                idx = (i + nums[i]) % n;
+                res[i] = nums[idx];
+
+            } else if (nums[i] < 0) {
+                idx = ((i + nums[i]) % n + n) % n;
+                res[i] = nums[idx];
+
+            } else {
+                res[i] == nums[i];
+            }
         }
         return res;
     }
