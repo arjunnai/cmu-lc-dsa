8fffca8b1b26b2efda093f626459d5f41402bba1
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-09 00:32:47 -0500

    [.cpp] 02-09-2026 , 1 ms (45.93%) , 9.9 MB (77.13%)

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string result = "";
-        for (char c : s) {
-            if (isalnum(static_cast<unsigned char>(c))) {
-                result += c;
-            }
-        }
-        transform(result.begin(), result.end(), result.begin(),
-                  [](unsigned char c) { return tolower(c); });
-        int left = 0, right = result.size() - 1;
+        // string result = "";
+        // for (char c : s) {
+        //     if (isalnum(static_cast<unsigned char>(c))) {
+        //         result += c;
+        //     }
+        // }
+        // transform(result.begin(), result.end(), result.begin(),
+        //           [](unsigned char c) { return tolower(c); });
+        // int left = 0, right = result.size() - 1;
+        // while (left <= right) {
+        //     if (result[left] != result[right]) {
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+        // }
+        // return true;
+        int left = 0, right = s.size() - 1;
         while (left <= right) {
-            if (result[left] != result[right]) {
+            if (!isalnum(static_cast<unsigned char>(s[left])))  {
+                left++;
+                continue;
+            }else if(!isalnum(static_cast<unsigned char>(s[right]))){
+                right--;
+                continue;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
