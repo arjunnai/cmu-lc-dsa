c04f8ac105c0a7c61dad92a8b2cd862f79dd18ff
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-13 12:18:11 -0500

    [.cpp] 02-13-2026 , 72 ms (65.59%) , 173 MB (80.48%)

--- LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
@@ -8,11 +8,10 @@ private:
         Node(int k = 0, int v = 0)
             : key(k), val(v), next(nullptr), prev(nullptr) {}
     };
-
     unordered_map<int, Node*> cache;
-    int cap;
     Node* head;
     Node* tail;
+    int cap;
 
 public:
     LRUCache(int capacity) {
@@ -25,46 +24,28 @@ public:
 
     int get(int key) {
         if (cache.contains(key)) {
-            // first get node* from the map
             Node* temp = cache[key];
-            // remove it from current spot
             removeNode(temp);
-            // add it to front of list
             addNode(temp);
-            // return value
             return temp->val;
-            // update hashmap - (No action needed: pointer address is unchanged)
         }
         return -1;
     }
 
     void put(int key, int value) {
-        // 2 scenarios
-        // 1. update - key already exists
         if (cache.contains(key)) {
-            // find the node
-            Node* existing = cache[key];
-            // update the value w new value
-            existing->val = value;
-            // remove old one from list
-            removeNode(existing);
-            // add to head of list since it becomes mru
-            addNode(existing);
+            Node* exist = cache[key];
+            exist->val = value;
+            removeNode(exist);
+            addNode(exist);
             return;
         }
-        // 2. key is new
-        Node* lru = tail->prev;
         if (cache.size() == cap) {
-            // id the node to remove
-            // get key from hashmap
-            //  remove key from hashmap
+            Node* lru = tail->prev;
             cache.erase(lru->key);
-            // removenode from list
             removeNode(lru);
-            // free memory
             delete lru;
         }
-        // add new node
         Node* newNode = new Node(key, value);
         cache[key] = newNode;
         addNode(newNode);
@@ -72,16 +53,17 @@ public:
 
     void addNode(Node* newNode) {
         Node* temp = head->next;
-        newNode->prev = head;
-        newNode->next = temp;
         head->next = newNode;
+        newNode->next = temp;
+        newNode->prev = head;
         temp->prev = newNode;
     }
+
     void removeNode(Node* delNode) {
-        Node* Ltemp = delNode->prev;
-        Node* Rtemp = delNode->next;
-        Ltemp->next = Rtemp;
-        Rtemp->prev = Ltemp;
+        Node* lTemp = delNode->prev;
+        Node* rTemp = delNode->next;
+        lTemp->next = rTemp;
+        rTemp->prev = lTemp;
     }
 };
 
