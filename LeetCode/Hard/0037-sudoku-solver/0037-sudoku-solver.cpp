ed079684782b3cfd9a37635a742c4d8ed0add3a9
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-16 20:21:16 -0500

    [.cpp] 02-16-2026 , 383 ms (32.04%) , 8.8 MB (34.69%)

---
+++ LeetCode/Hard/0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solveSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(board[i][j] == '.'){
+                    for(char c = '1' ; c <= '9'; c++){
+                        if(isValid(board, i , j , c)){
+                            board[i][j] = c;
+                            if(solveSudoku(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c || board[i][col] == c ||
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
