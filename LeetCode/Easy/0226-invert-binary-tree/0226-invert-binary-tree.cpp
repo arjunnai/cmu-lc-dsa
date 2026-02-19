ae937561828d606e97266c1ca15e3115c888b0eb
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-02-19 01:29:32 -0500

    Time: 0 ms (100%), Space: 12.3 MB (86.73%) - LeetHub

--- LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -6,15 +6,15 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr){
+        if (!root)
             return nullptr;
-        }
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
