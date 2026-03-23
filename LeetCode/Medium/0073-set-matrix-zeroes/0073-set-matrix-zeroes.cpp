719668ef47d78fdb4b4e8edfd76c29d6d522eda3
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-03-23 19:44:02 -0400

    Time: 3 ms (15.79%), Space: 20.7 MB (25.07%) - LeetHub

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool rowZ = false;
-
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<bool> rowdead(rows, false);
+        vector<bool> coldead(cols, false);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    if (i > 0) {
-                        matrix[i][0] = 0;
-                    } else {
-                        rowZ = true;
-                    }
+                    rowdead[i] = true;
+                    coldead[j] = true;
                 }
             }
         }
-        for (int i = 1; i < matrix.size(); i++) {
-            for (int j = 1; j < matrix[0].size(); j++) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rowdead[i] == true || coldead[j] == true) {
                     matrix[i][j] = 0;
                 }
-            }
-        }
-        if (matrix[0][0] == 0) {
-            for (int i = 1; i < matrix.size(); i++) {
-                matrix[i][0] = 0;
-            }
-        }
-        if (rowZ) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                matrix[0][j] = 0;
             }
         }
     }
