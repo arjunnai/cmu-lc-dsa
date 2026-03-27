3c253bb4b1d782d151c6c940ced714cade5ad376
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-03-26 20:52:13 -0400

    Time: 3 ms (89.39%), Space: 23.9 MB (58.79%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> merged;
-        if (intervals.empty()) {
-                return {};
-            }
-        else {
-            merged.push_back(intervals[0]) ;
-            for (int i = 1; i < intervals.size(); i++) {
-                if (merged.back()[1] >= intervals[i][0]) {
-                    merged.back()[1] = max(merged.back()[1], intervals[i][1]);
-                }else{
-                    merged.push_back(intervals[i]);
-                }
+        vector<vector<int>> ans;
+        int l1 = intervals[0][0];
+        int r1 = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); i++) {
+            int l2 = intervals[i][0];
+            int r2 = intervals[i][1];
+            if (l2 <= r1) {
+                r1 = max(r1, r2);
+            } else {
+                ans.push_back({l1, r1});
+                l1 = l2;
+                r1 = r2;
             }
         }
-        return merged;
+        ans.push_back({l1, r1});
+        return ans;
     }
 };
+
+// l1 r1 l2 r2
+// 1  3  2  6
