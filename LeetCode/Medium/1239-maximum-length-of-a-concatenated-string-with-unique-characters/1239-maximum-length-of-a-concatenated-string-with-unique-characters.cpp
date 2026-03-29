b38fb58a576c2b4d95c7d8ce3e635f7a0660d447
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-03-29 16:03:34 -0400

    Time: 19 ms (65.35%), Space: 26.9 MB (59.94%) - LeetHub

--- LeetCode/Medium/1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ LeetCode/Medium/1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,35 +1,52 @@
 class Solution {
-public:
-    int maxSize = 0;
-
-    int maxLength(vector<string>& arr) {
-        dfs(arr, 0, arr.size(), "");
-        return maxSize;
-    }
-
-    void dfs(vector<string>& arr, int i, int n, string curr) {
-        if (is_unique(curr) == false)
-            return;
-        if (i == n) {
-            if (curr.size() > maxSize) {
-                int currSize = curr.size();
-                maxSize = max(maxSize, currSize);
-            }
-            return;
+    bool check(vector<int>& track, string& s) {
+        // self check
+        vector<int> self(26, 0);
+        for (auto i : s) {
+            if (self[i - 'a'])
+                return false;
+            self[i - 'a'] = 1;
         }
-        dfs(arr, i + 1, n, curr + arr[i]);
-        dfs(arr, i + 1, n, curr);
-    }
-    bool is_unique(string& str) {
-        int n = str.size();
-        // ump stores the count of all the characters in string
-        vector<int> ump(26, 0);
-        for (int i = 0; i < n; i++) {
-            ump[str[i] - 'a']++;
-            // if we find duplicate return false
-            if (ump[str[i] - 'a'] > 1)
+
+        // track check
+        for (auto i : s) {
+            if (track[i - 'a'])
                 return false;
         }
         return true;
+    }
+    void dfs(int& ans, int index, vector<int>& track, vector<string>& arr,
+             int len) {
+
+        // base case
+        if (index >= arr.size()) {
+            ans = max(ans, len);
+            return;
+        }
+        // dfs begins - we need to check if we can take the string or not
+        if (check(track, arr[index])) {
+            // this means it came true and we have to either include or exclude
+            // it include
+            for (auto i : arr[index]) {
+                track[i - 'a'] = 1;
+            }
+            dfs(ans, index + 1, track, arr, len + arr[index].size());
+
+            // exclude
+            for (auto i : arr[index]) {
+                track[i - 'a'] = 0;
+            }
+            dfs(ans, index + 1, track, arr, len);
+        } else {
+            dfs(ans, index + 1, track, arr, len);
+        }
+    }
+
+public:
+    int maxLength(vector<string>& arr) {
+        int ans = INT_MIN, index = 0;
+        vector<int> track(26, 0);
+        dfs(ans, index, track, arr, 0);
+        return ans;
     }
 };
