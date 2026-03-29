cb0a8067bbfa90299b2d3157fc035c212af6032a
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-03-29 01:05:21 -0400

    Time: 3 ms (32.02%), Space: 10.3 MB (96.71%) - LeetHub

--- LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> dup;
         sort(nums.begin(), nums.end());
-        dfs(nums, 0, dup, res);
-        return res;
+        vector<vector<int>> ans;
+        vector<int> subset;
+        dfs(nums, ans, subset, 0);
+        return ans;
     }
-    void dfs(vector<int>& nums, int start, vector<int>& curr,
-             vector<vector<int>>& result) {
-        result.push_back(curr);
-        for (int i = start; i < nums.size(); i++) {
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            curr.push_back(nums[i]);
-            dfs(nums, i + 1, curr, result);
-            curr.pop_back();
+
+    void dfs(vector<int>& nums, vector<vector<int>>& ans, vector<int>& subset,
+             int i) {
+        if (i == nums.size()) {
+            ans.push_back(subset);
+            return;
         }
+        subset.push_back(nums[i]);
+        dfs(nums, ans, subset, i + 1);
+        subset.pop_back();
+        //if we exclude a number, we exlude every copy of that number
+        
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            i++;
+        }
+        dfs(nums, ans, subset, i + 1);
     }
 };
