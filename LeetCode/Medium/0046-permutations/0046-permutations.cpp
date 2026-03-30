912446bfe510e1e001b9272de1efe3121e1b13ed
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-03-29 20:31:20 -0400

    Time: 0 ms (100%), Space: 10.7 MB (37.88%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.cpp
+++ LeetCode/Medium/0046-permutations/0046-permutations.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> curr;
         vector<bool> used(nums.size(), false);
-        dfs(res, curr, used, nums);
+        vector<int> curr;
+        dfs(nums, res, used, curr);
         return res;
     }
-    void dfs(vector<vector<int>>& res, vector<int>& curr, vector<bool>& used,
-             vector<int>& nums) {
-        // base case
+
+    void dfs(vector<int>& nums, vector<vector<int>>& res, vector<bool>& used,
+             vector<int>& curr) {
+        // base case if curr.size and nums size is same
         if (curr.size() == nums.size()) {
             res.push_back(curr);
             return;
         }
+
+        // loop similar to combination
         for (int i = 0; i < nums.size(); i++) {
-            if (used[i] == true) {
+            if (used[i] == true)
                 continue;
-            }
             used[i] = true;
             curr.push_back(nums[i]);
-            dfs(res, curr, used, nums);
-            used[i] = false;
+            dfs(nums, res, used, curr);
             curr.pop_back();
+            used[i] = false;
         }
     }
 };
