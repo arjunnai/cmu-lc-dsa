fa05b2d7c3599f50a96343887f51dbad18ab460f
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-04 18:22:37 -0400

    Time: 0 ms (100%), Space: 13.1 MB (78.08%) - LeetHub

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        dfs(n, n, "", res);
+        string curr = "";
+        dfs(n, n, curr, res);
         return res;
     }
-    void dfs(int o, int c, string curr, vector<string>& res){
-        if(o == c && o == 0 && c == 0){
+    void dfs(int o, int c, string& curr, vector<string>& res){
+        if(o == 0 && c == 0){
             res.push_back(curr);
             return;
         }
         if(o > 0){
-            dfs(o-1, c, curr+"(", res);
+            //can either use push_back or directly call curr + "(" in the fucntion, if using push_back, have to pop_back also
+            curr.push_back('(');
+            dfs(o-1, c, curr, res);
+            curr.pop_back();
         }
         if(o < c){
-            dfs(o, c-1, curr+")", res);
+            curr.push_back(')');
+            dfs(o, c-1, curr, res);
+            curr.pop_back();
         }
     }
 };
