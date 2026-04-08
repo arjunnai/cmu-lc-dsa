95711b77c8ec6706c06b7ba808774e5cbfa4f7d3
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-07 21:03:12 -0400

    Time: 0 ms (100%), Space: 27.6 MB (68.79%) - LeetHub

--- LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.cpp
+++ LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0, area = 0, rows = grid.size(), cols = grid[0].size();
+        int area = 0, maxArea = 0;
+        int rows = grid.size(), cols = grid[0].size();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
                     area = 0;
-                    dfs(grid, area, i, j);
+                    dfs(grid, i, j, area);
+                    maxArea = max(maxArea, area);
                 }
-                maxArea = max(maxArea, area);
             }
         }
         return maxArea;
     }
-    void dfs(vector<vector<int>>& grid, int& area, int i, int j) {
-        int rows = grid.size(), cols = grid[0].size();
-        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] != 1) {
+    void dfs(vector<vector<int>>& grid, int i, int j, int& area) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() ||
+            grid[i][j] != 1) {
             return;
         }
+        grid[i][j] = '*';
         area++;
-        grid[i][j] = 0;
-        dfs(grid, area, i + 1, j);
-        dfs(grid, area, i, j + 1);
-        dfs(grid, area, i - 1, j);
-        dfs(grid, area, i, j - 1);
+        dfs(grid, i + 1, j, area);
+        dfs(grid, i - 1, j, area);
+        dfs(grid, i, j - 1, area);
+        dfs(grid, i, j + 1, area);
     }
 };
