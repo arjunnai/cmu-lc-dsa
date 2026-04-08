e50c21c47d818570058c89b7d088f22a1cd2093a
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-07 20:53:29 -0400

    Time: 30 ms (34%), Space: 16.2 MB (88.95%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
+        int res = 0;
+        int rows = grid.size(), cols = grid[0].size();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == '1') {
-
                     dfs(grid, i, j);
-                    count++;
+                    res++;
                 }
             }
         }
-        return count;
+        return res;
     }
     void dfs(vector<vector<char>>& grid, int i, int j) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] != '1') {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() ||
+            grid[i][j] != '1') {
             return;
         }
+        //mark as visited so we dont recount
         grid[i][j] = '*';
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
+        //go in all 4 directiosn
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
     }
 };
