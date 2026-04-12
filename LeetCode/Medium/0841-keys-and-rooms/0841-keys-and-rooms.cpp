e0829ea7c07c08c628a2066233ca50beabd1037f
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-12 18:06:34 -0400

    Time: 0 ms (100%), Space: 14.4 MB (88.51%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<bool> visited(rooms.size(), false);
+        // bfs
+        int n = rooms.size();
+        vector<bool> visited(n, false);
+        // queue<int> q;
+
+        // // visit 0th room
+        // visited[0] = true;
+        // q.push(0);
+
+        // int count = 1;
+        // while (!q.empty()) {
+        //     int curr = q.front();
+        //     q.pop();
+        //     for (int key : rooms[curr]) {
+        //         if (!visited[key]) {
+        //             visited[key] = true;
+        //             q.push(key);
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count == n;
+
+        // dfs
         dfs(0, rooms, visited);
         for (bool v : visited) {
-            if (!v) {
+            if (!v)
                 return false;
-            }
         }
         return true;
     }
-    void dfs(int currentRoom, vector<vector<int>>& rooms,
-             vector<bool>& visited) {
-        visited[currentRoom] = true;
-        for (int i = 0; i < rooms[currentRoom].size(); i++) {
-            if (!visited[rooms[currentRoom][i]]) {
-                dfs(rooms[currentRoom][i], rooms, visited);
-            }
+    void dfs(int room, vector<vector<int>>& rooms, vector<bool>& visited) {
+        if (visited[room] == true)
+            return;
+        visited[room] = true;
+
+        for (int key : rooms[room]) {
+            dfs(key, rooms, visited);
         }
     }
 };
