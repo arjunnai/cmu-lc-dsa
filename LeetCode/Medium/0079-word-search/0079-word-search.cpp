e0688934fbbbb2ffbaefe6b8797da379c4b35ad6
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-12 21:04:49 -0400

    Time: 926 ms (8.39%), Space: 11.8 MB (13.59%) - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.cpp
+++ LeetCode/Medium/0079-word-search/0079-word-search.cpp
@@ -2,45 +2,41 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (board[row][col] == word[0]) {
-                    if (dfs(board, word, row, col, 0))
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, i, j, 0, visited)) {
                         return true;
+                    }
                 }
             }
         }
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board, string& word, int row, int col,
-             int idx) {
-        // base case : reach end of the word
-        if (idx == word.size()) {
-            return true;
-        }
-        // base case bad : out of bounds, lettter doesn't match target or *
-        // already visited
-        if (row >= board.size() || row < 0 || col >= board[0].size() || col < 0 ||
-            board[row][col] != word[idx]) {
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int idx,
+             vector<vector<int>>& visited) {
+        // base case - outbound and inbound
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) {
             return false;
         }
-
-        // visited hack - mark position as *
-        char tempVisited = board[row][col];
-        board[row][col] = '*';
-
-        // create bool, call dfs on 4 directions - up, down, left, right, if any
-        // true - variable is true
-        bool found = dfs(board, word, row - 1, col, idx + 1) ||
-                     dfs(board, word, row + 1, col, idx + 1) ||
-                     dfs(board, word, row, col - 1, idx + 1) ||
-                     dfs(board, word, row, col + 1, idx + 1);
-
+        if (visited[i][j] == 1 || board[i][j] != word[idx]) {
+            return false;
+        }
+        if (idx == word.size() - 1) {
+            return true;
+        }
+        visited[i][j] = 1;
+        if (dfs(board, word, i - 1, j, idx + 1, visited) ||
+            dfs(board, word, i + 1, j, idx + 1, visited) ||
+            dfs(board, word, i, j - 1, idx + 1, visited) ||
+            dfs(board, word, i, j + 1, idx + 1, visited)) {
+            return true;
+        }
         // backtrack
-        board[row][col] = tempVisited;
-
-        // return bool variable
-        return found;
+        visited[i][j] = 0;
+        return false;
     }
 };
