2587643f519cfaf5a298a980e3c3273ec5887f0f
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-12 21:07:19 -0400

    Time: 183 ms (92.9%), Space: 10.5 MB (82.26%) - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.cpp
+++ LeetCode/Medium/0079-word-search/0079-word-search.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
+        // vector<vector<int>> visited(m, vector<int>(n, 0));
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word[0]) {
-                    if (dfs(board, word, i, j, 0, visited)) {
+                    if (dfs(board, word, i, j, 0)) {
                         return true;
                     }
                 }
@@ -16,27 +16,26 @@ public:
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board, string word, int i, int j, int idx,
-             vector<vector<int>>& visited) {
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx) {
         // base case - outbound and inbound
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) {
-            return false;
-        }
-        if (visited[i][j] == 1 || board[i][j] != word[idx]) {
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[idx]) {
             return false;
         }
         if (idx == word.size() - 1) {
             return true;
         }
-        visited[i][j] = 1;
-        if (dfs(board, word, i - 1, j, idx + 1, visited) ||
-            dfs(board, word, i + 1, j, idx + 1, visited) ||
-            dfs(board, word, i, j - 1, idx + 1, visited) ||
-            dfs(board, word, i, j + 1, idx + 1, visited)) {
+        char temp = board[i][j];
+        board[i][j] = '#';
+        // visited[i][j] = 1;
+        if (dfs(board, word, i - 1, j, idx + 1) ||
+            dfs(board, word, i + 1, j, idx + 1) ||
+            dfs(board, word, i, j - 1, idx + 1) ||
+            dfs(board, word, i, j + 1, idx + 1)) {
             return true;
         }
         // backtrack
-        visited[i][j] = 0;
+        // visited[i][j] = 0;
+        board[i][j] = temp;
         return false;
     }
 };
