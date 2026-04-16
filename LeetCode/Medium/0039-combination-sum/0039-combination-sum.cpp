f1489b3efad66508a25b2e7cdf2d8bcb9388c1a4
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-04-16 01:30:52 -0400

    Time: 0 ms (100%), Space: 14.1 MB (52.05%) - LeetHub

--- LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // dfs 2 options
-        //  1 - include candidates[i], add num to curr, target - candidates[i],
-        //  dfs(i), pop 2 - exclude candidates[i], curr unchanged, target
-        //  unchanged, dfs(i+1)
-        vector<vector<int>> res;
+        vector<vector<int>> ans;
         vector<int> curr;
-        dfs(res, curr, candidates, target, 0);
-        return res;
+        dfs(candidates, target, curr, ans, 0);
+        return ans;
     }
-    void dfs(vector<vector<int>>& res, vector<int>& curr,
-             vector<int>& candidates, int target, int i) {
-        if (target == 0) { // success
-            res.push_back(curr);
+
+    void dfs(vector<int>& candidates, int target, vector<int>& curr,
+             vector<vector<int>>& ans, int idx) {
+        // dfs either include candidates[idx] or dont include it
+        if (target == 0) {
+            ans.push_back(curr);
             return;
-        } else if (i == candidates.size() || target < 0) { // failure
+        } else if (idx == candidates.size() || target < 0) {
             return;
         }
-        curr.push_back(candidates[i]);
-        dfs(res, curr, candidates, (target - candidates[i]), i);
+        curr.push_back(candidates[idx]);
+        dfs(candidates, target - candidates[idx], curr, ans, idx);
+        // backtrack
         curr.pop_back();
-        dfs(res, curr, candidates, target, i + 1);
+        dfs(candidates, target, curr, ans, idx + 1);
     }
 };
