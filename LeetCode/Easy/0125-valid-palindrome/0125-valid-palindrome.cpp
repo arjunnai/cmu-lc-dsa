273b82910aecf369094d3b152e5491988dd683a7
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-04 16:40:32 -0400

    Time: 2 ms (36.62%), Space: 10 MB (54.33%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // string result = "";
-        // for (char c : s) {
-        //     if (isalnum(static_cast<unsigned char>(c))) {
-        //         result += c;
-        //     }
-        // }
-        // transform(result.begin(), result.end(), result.begin(),
-        //           [](unsigned char c) { return tolower(c); });
-        // int left = 0, right = result.size() - 1;
-        // while (left <= right) {
-        //     if (result[left] != result[right]) {
-        //         return false;
-        //     }
-        //     left++;
-        //     right--;
-        // }
-        // return true;
-        int left = 0, right = s.size() - 1;
-        while (left <= right) {
-            if (!isalnum(static_cast<unsigned char>(s[left])))  {
-                left++;
-                continue;
-            }else if(!isalnum(static_cast<unsigned char>(s[right]))){
-                right--;
-                continue;
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) {
+                l++;
             }
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
+            while (l < r && !isalnum(s[r])) {
+                r--;
             }
-            left++;
-            right--;
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false; // Found a mismatch
+            }
+            l++;
+            r--;
         }
         return true;
     }
