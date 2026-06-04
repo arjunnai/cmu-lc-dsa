e4de4cdf0755cd0be127bdd87c9787974fda83a3
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-04 16:41:52 -0400

    Time: 0 ms (100%), Space: 9.9 MB (77.62%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int l = 0, r = s.size() - 1;
         while (l < r) {
             while (l < r && !isalnum(s[l])) {
