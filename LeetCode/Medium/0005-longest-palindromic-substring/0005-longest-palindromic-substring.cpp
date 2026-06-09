4c3a8fefce42037a3316588fac505caa6339edf7
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-09 17:39:48 -0400

    Time: 7 ms (87.36%), Space: 9.4 MB (65.42%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,25 +1,57 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int longest = 0;
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);
-            int len2 = expand(s, i, i + 1);
+        //     int l = 0, r = s.size() - 1;
+        //     string longest = "";
+        //     // while (l < r) {
+        //     //     if (isPali(s, l, r) && (r - l + 1) > longest.size()) {
+        //     //         longest = s.substr(l, r - i + 1);
+        //     //     }
+        //     //     l++;
+        //     //     r--;
+        //     // }
+        //     for (int i = 0; i < s.size(); i++) {
+        //         for (int j = i; j < s.size(); j++) {
+        //             if (isPali(s, i, j) && (j - i + 1) > longest.size()) {
+        //                 longest = s.substr(i, j - i + 1);
+        //             }
+        //         }
+        //     }
+        //     return longest;
+        // }
+        // bool isPali(string s, int l, int r) {
+        //     while (l < r) {
+        //         if (s[l] != s[r]) {
+        //             return false;
+        //         }
+        //         l++;
+        //         r--;
+        //     }
+        //     return true;
+        // }
+        // o(n3) above
+
+        if (s.size() < 1)
+            return "";
+        int start = 0, maxLen = 0;
+
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < s.size() && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            return r - l - 1;
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expand(i, i);
+            int len2 = expand(i, i + 1);
             int len = max(len1, len2);
-            if (len > longest) {
-                longest = len;
+            if (len > maxLen) {
+                maxLen = len;
                 start = i - (len - 1) / 2;
             }
         }
-        return s.substr(start, longest);
-    }
-    int expand(string& s, int left, int right) {
-
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            left--;
-            right++;
+            return s.substr(start, maxLen);
         }
-        return right - left - 1;
-    }
-};
+    };
