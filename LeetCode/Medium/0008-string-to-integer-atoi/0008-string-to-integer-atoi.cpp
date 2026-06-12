944b807d13e50e4691c9651e7b1774c2bd2113eb
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-12 15:35:24 -0400

    Time: 0 ms (100%), Space: 9.3 MB (11.1%) - LeetHub

--- LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -17,6 +17,7 @@ public:
         while (i < n && isdigit(s[i])) {
             int digit = s[i] - '0';
             // check overflow before updating res
+            // int = 2147483647 - ends in 7, so /10 gives you 214748364 we do *10 so it will overflow  if its equal we check if digit coming in is 7 or lower then its safe else overflow
             if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7)) {
                 return neg ? INT_MIN : INT_MAX;
             }
