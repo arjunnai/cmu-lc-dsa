6e3f4590987a042f83e8aec05fbdcadf72064978
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-15 15:44:18 -0400

    Time: 2 ms (66.68%), Space: 9.6 MB (57.95%) - LeetHub

---
+++ LeetCode/Medium/0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0")
+            return "0";
+        int n = num1.size(), m = num2.size();
+        vector<int> res(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + res[p2];
+                res[p2] = sum % 10;
+                res[p1] += sum / 10;
+            }
+        }
+        string st = "";
+        for (int i = 0; i < res.size(); i++) {
+            if (i == 0 && res[i] == 0)
+                continue;
+            st += (res[i] + '0');
+        }
+        return st;
+    }
+};
