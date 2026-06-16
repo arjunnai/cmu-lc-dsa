4cf5a960be9995d48989e74c60507392c6185515
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-15 22:54:12 -0400

    Time: 0 ms (100%), Space: 12.3 MB (40.78%) - LeetHub

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int n1 = m - 1, n2 = n - 1, write_index = m + n - 1;
-        while (n1 >= 0 && n2 >= 0) {
-            if (nums1[n1] > nums2[n2]) {
-                nums1[write_index] = nums1[n1];
-                n1--;
-                write_index--;
+        int m1 = m - 1, n1 = n - 1, widx = nums1.size() - 1;
+        while (m1 >= 0 && n1 >= 0) {
+            if (nums1[m1] >= nums2[n1]) {
+                nums1[widx] = nums1[m1];
+                m1--;
+                widx--;
             } else {
-                nums1[write_index] = nums2[n2];
-                n2--;
-                write_index--;
+                nums1[widx] = nums2[n1];
+                n1--;
+                widx--;
             }
         }
-
-        while(n2>=0){
-            nums1[write_index] = nums2[n2];
-            n2--;
-            write_index--;
+        while (n1 >= 0) {
+            nums1[widx] = nums2[n1];
+            n1--;
+            widx--;
         }
     }
 };
