d9db5d11664049897a20af13ddf0a90a148f517b
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-16 23:17:41 -0400

    Time: 0 ms (100%), Space: 36 MB (65.21%) - LeetHub

--- LeetCode/Medium/0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ LeetCode/Medium/0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -4,17 +4,12 @@ public:
         int l = 0, r = arr.size() - k;
         while (l < r) {
             int mid = l + (r - l) / 2;
-            // Check if starting at mid is valid (arr[mid] is closer or equal to
-            // arr[mid+k])
             if (x - arr[mid] > arr[mid + k] - x) {
-                // arr[mid + k] is closer, should start after mid (moving window right)
                 l = mid + 1;
             } else {
-                // arr[mid] is closer or equal, this is a valid start
                 r = mid;
             }
         }
-        // l and r at best starting index
         return vector<int>(arr.begin() + l, arr.begin() + l + k);
     }
 };
