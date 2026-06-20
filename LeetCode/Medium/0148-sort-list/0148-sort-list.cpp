a53da198ebdc52a1ef4d72a86d90e8aae13ee56a
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-19 23:11:31 -0400

    Time: 14 ms (66.94%), Space: 57 MB (81.42%) - LeetHub

--- LeetCode/Medium/0148-sort-list/0148-sort-list.cpp
+++ LeetCode/Medium/0148-sort-list/0148-sort-list.cpp
@@ -10,37 +10,45 @@
  */
 class Solution {
 public:
-    // merge sort - first do tortoise hare and find midpoint, then cut the
-    // linkedlist by using prev pointer
-    // then run sortList twice on left half and right half
-    // then do 2 sorted list questoin
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        //attach remains
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
     ListNode* sortList(ListNode* head) {
+        // split
+        // recurse
+        // merge (compare and merge
         if (!head || !head->next)
             return head;
-        ListNode *slow = head, *fast = head;
-        ListNode* prev = nullptr;
-        while (fast != nullptr && fast->next != nullptr) {
-            prev = slow;
-            slow = slow->next;
 
+        ListNode *slow = head, *fast = head->next;
+        ;
+        while (fast && fast->next) {
+            slow = slow->next;
             fast = fast->next->next;
         }
-        prev->next = nullptr;
-        ListNode* leftSort = sortList(head);
-        ListNode* rightSort = sortList(slow);
-        ListNode dummy(0);
-        ListNode *curr = &dummy;
-        while (leftSort && rightSort) {
-            if (leftSort->val < rightSort->val) {
-                curr->next = leftSort;
-                leftSort = leftSort->next;
-            } else {
-                curr->next = rightSort;
-                rightSort = rightSort->next;
-            }
-            curr = curr->next;
-        }
-        curr->next = (leftSort != nullptr) ? leftSort : rightSort;
-        return dummy.next;
+        ListNode* mid = slow->next;
+        // this cuts it into 2 halves
+        slow->next = nullptr;
+
+        // recurse - sort each half
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+
+        return merge(left, right);
     }
 };
