455915dbe317330356841ddaeaf942e8d181e731
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-06-22 16:09:41 -0400

    Time: 3 ms (35.61%), Space: 11.4 MB (17.15%) - LeetHub

--- LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        //stringstream in cpp used to remove spcaes and make string as words
         stringstream ss(s);
-        string word;
-        vector<string> words;
-
-        while(ss >> word){
-            words.push_back(word);
+        string words;
+        vector<string> ans;
+        while (ss >> words) {
+            ans.push_back(words);
         }
-
-        reverse(words.begin(), words.end());
-
-        string res = "";
-        for(int i = 0 ; i < words.size(); i++){
-            res+=words[i];
-            //if current word is not last word add space
-            if(i != words.size()-1){
-                res += " ";
-            }
+        reverse(ans.begin(), ans.end());
+        string result = "";
+        for (int i = 0; i < ans.size(); i++) {
+            if (i > 0)
+                result += " ";
+            result += ans[i];
         }
-        return res;
+        return result;
     }
 };
