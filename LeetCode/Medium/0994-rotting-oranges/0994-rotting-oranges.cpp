8fd1962f82a3e08697accbef2d77e0160213e578
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-07-09 15:33:15 -0400

    Time: 0 ms (100%), Space: 16.7 MB (86.37%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
+    typedef pair<int, int> P;
+    vector<pair<int, int>> dirs{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> rotten;
-        int m = grid.size(), n = grid[0].size(), mins = 0, fresh = 0;
+        int m = grid.size(), n = grid[0].size(), fresh = 0, mins = 0;
+        queue<P> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2)
-                    rotten.push({i, j});
-                else if (grid[i][j] == 1)
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
                     fresh++;
+                }
             }
         }
-        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!rotten.empty() && fresh > 0) {
-            int rotlevelsize = rotten.size();
-            for (int k = 0; k < rotlevelsize; k++) {
-                auto [i, j] = rotten.front();
-                rotten.pop();
+        if (fresh == 0)
+            return 0;
+        while (!q.empty()) {
+            int qsize = q.size();
+            while (qsize--) {
+                P curr = q.front();
+                q.pop();
+                int i = curr.first, j = curr.second;
                 for (auto& d : dirs) {
-                    int ni = i + d[0], nj = j + d[1];
-                    if (ni < 0 || ni >= m || nj < 0 || nj >= n ||
-                        grid[ni][nj] != 1)
+                    int ni = i + d.first, nj = j + d.second;
+                    if (ni < 0 || ni >= m || nj < 0 || nj >= n)
+                        continue;
+                    if (grid[ni][nj] != 1)
                         continue;
                     grid[ni][nj] = 2;
+                    q.push({ni, nj});
                     fresh--;
-                    rotten.push({ni, nj});
                 }
             }
             mins++;
         }
-        return fresh == 0 ? mins : -1;
+        return fresh == 0 ? mins - 1 : -1;
     }
 };
