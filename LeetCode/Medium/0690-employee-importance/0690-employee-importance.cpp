ee1b6afe2f74fe8276bf6848262d8f6beeb38d76
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-07-15 19:21:17 -0400

    Time: 19 ms (91.72%), Space: 21.1 MB (62.85%) - LeetHub

---
+++ LeetCode/Medium/0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,27 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> m;
+        for (auto x : employees)
+            m[x->id] = x;
+        int sum = 0;
+        dfs(m, id, sum);
+        return sum;
+    }
+
+    void dfs(unordered_map<int, Employee*>& m, int id, int& sum) {
+        sum += m[id]->importance;
+        for (auto x : m[id]->subordinates)
+            dfs(m, x, sum);
+    }
+};
