fd141b4ed88aaa91ce53f92848a0ad1659a46aba
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-08-03 02:18:45 -0400

    Time: 0 ms (100%), Space: 9.4 MB (79.65%) - LeetHub

--- LeetCode/Easy/1114-print-in-order/1114-print-in-order.cpp
+++ LeetCode/Easy/1114-print-in-order/1114-print-in-order.cpp
@@ -10,12 +10,13 @@ public:
 
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
+        std::lock_guard<std::mutex> lk(m);
         next = 2;
         cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
-        std::unique_lock glock(m);
+        std::unique_lock<std::mutex> glock(m);
         cv.wait(glock, [this]() { return next == 2; });
         // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
@@ -26,7 +27,7 @@ public:
     void third(function<void()> printThird) {
 
         // printThird() outputs "third". Do not change or remove this line.
-        std::unique_lock lk(m);
+        std::unique_lock<std::mutex> lk(m);
         cv.wait(lk, [this]() { return next == 3; });
         printThird();
     }
