1a379303027bfe01730b8c51f12134d7f9559593
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-08-03 20:45:17 -0400

    Time: 23 ms (98.01%), Space: 53.4 MB (83.99%) - LeetHub

--- LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,26 +3,10 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> curr;
-        dfs(s, ans, curr, 0);
+        dfs(s, 0, ans, curr);
         return ans;
     }
-    void dfs(string s, vector<vector<string>>& ans, vector<string>& curr,
-             int start) {
-        if (start == s.size()) {
-            ans.push_back(curr);
-            return;
-        }
-
-        for (int end = start; end < s.size(); end++) {
-            if (isPalindrome(s, start, end)) {
-                curr.push_back(s.substr(start, end - start + 1));
-                dfs(s, ans, curr, end + 1);
-                curr.pop_back();
-            }
-        }
-    }
-
-    bool isPalindrome(string s, int l, int r) {
+    bool isPali(string& s, int l, int r) {
         while (l <= r) {
             if (s[l] != s[r]) {
                 return false;
@@ -31,5 +15,21 @@ public:
             r--;
         }
         return true;
+    }
+
+    void dfs(string& s, int idx, vector<vector<string>>& ans,
+             vector<string>& curr) {
+        if (idx == s.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = idx; i < s.size(); i++) {
+            if (isPali(s, idx, i)) {
+                curr.push_back(s.substr(idx, i - idx + 1));
+                dfs(s, i + 1, ans, curr);
+                curr.pop_back();
+            }
+        }
     }
 };
