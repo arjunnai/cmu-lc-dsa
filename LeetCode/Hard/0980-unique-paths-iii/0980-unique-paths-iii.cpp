ca6f25144eb3a66e49dced39322d7a5bf065fa83
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-08-05 00:25:42 -0400

    Time: 0 ms (100%), Space: 9.2 MB (74.12%) - LeetHub

--- LeetCode/Hard/0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ LeetCode/Hard/0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    int m, n, result, nonObstacle;
+    int m;
+    int n;
+    int zeros;
+    vector<vector<int>> dirs{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int uniquePathsIII(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        nonObstacle = 0, result = 0;
-        int startX = 0, startY = 0;
+        m = grid.size(), n = grid[0].size();
+        zeros = 0;
+        int result = 0;
+        int startx = 0, starty = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0)
-                    nonObstacle++;
-                if (grid[i][j] == 1) {
-                    startX = i;
-                    startY = j;
+                if (grid[i][j] == 0) {
+                    zeros++;
+                } else if (grid[i][j] == 1) {
+                    startx = i;
+                    starty = j;
                 }
             }
         }
-        nonObstacle += 1;
-        int count = 0;
-        backtrack(grid, count, startX, startY);
-        return result;
+
+        return dfs(startx, starty, grid);
     }
-    void backtrack(vector<vector<int>>& grid, int count, int i, int j) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == -1)
-            return;
-        if (grid[i][j] == 2) {
-            if (count == nonObstacle)
-                result++;
-            return;
+
+    int dfs(int i, int j, vector<vector<int>>& grid) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == -1) {
+            return 0;
         }
+        if (grid[i][j] == 2)
+            return zeros == 0 ? 1 : 0;
+
+        int temp = grid[i][j];
         grid[i][j] = -1;
+        if (temp == 0)
+            zeros--;
+
+        int total = 0;
         for (auto& d : dirs) {
-            int newi = i + d[0];
-            int newj = j + d[1];
-            backtrack(grid, count + 1, newi, newj);
+            int ni = i + d[0];
+            int nj = j + d[1];
+            total += dfs(ni, nj, grid);
         }
-        grid[i][j] = 0;
+        grid[i][j] = temp;
+        if (temp == 0)
+            zeros++;
+        return total;
     }
 };
