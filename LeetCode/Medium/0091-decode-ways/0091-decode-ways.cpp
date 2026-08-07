c19beffea4c0709d069c8a932a0c98e180672877
Arjun <50323916+arjunnai@users.noreply.github.com>
2026-08-06 22:04:47 -0400

    Time: 0 ms (100%), Space: 8.4 MB (88.38%) - LeetHub

---
+++ LeetCode/Medium/0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int t[101];
+    int dfs(int i, string& s, int& n) {
+        if (i == n)
+            return 1;
+        if (s[i] == '0')
+            return 0;
+
+        if (t[i] != -1)
+            return t[i];
+        int result = dfs(i + 1, s, n);
+        if (i + 1 < n) {
+            if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+                result += dfs(i + 2, s, n);
+            }
+        }
+        return t[i] = result;
+    }
+
+    int numDecodings(string s) {
+        int n = s.size();
+        memset(t, -1, sizeof(t));
+        return dfs(0, s, n);
+    }
+};
